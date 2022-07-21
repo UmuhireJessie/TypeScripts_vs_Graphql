1380fb2d34a1b0b40871dce843139d6971dc0bcd
UmuhireJessie <j.umuhire@alustudent.com>
2022-07-21 15:24:54 +0200

    2nd lesson

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,15 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+let num = [];
+numbers.forEach(n => n.toExponential);
+let user = [1, 'Jessie'];
+user.push(1);
+console.log(user);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-let age: number = 20;
-if (age < 50) age += 10;
-console.log(age);
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+
+function render(document: any) { //Or change the noImplicitAny in tsconfig file
+    console.log(document);
+}
+
+let numbers = [1, 2, 3]
+let num : number[] = []
+numbers.forEach(n => n.toExponential) // suggestion because we already know the type 
+
+let user: [number, string] = [1, 'Jessie']
+user.push(1); // in this case, it doesn't complain which is a gap that exist at the time
